265e6523ca395dc2780d236f727263bef9e3fb73
Yibo Fu <yibomus@gmail.com>
2023-06-30 02:34:58 -0700

    add more operations from sqlc

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,1 +1,43 @@
 package db
+
+import (
+	"context"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Unit Test for DB Operations")
+}
+
+var _ = Describe("Operation", func() {
+
+	BeforeEach(func() {
+
+	})
+
+	AfterEach(func() {
+
+	})
+
+	Context("customer functions", func() {
+		It("Test CreateAccountParams", func() {
+			arg := CreateAccountParams{
+				Owner:    "1",
+				Balance:  100,
+				Currency: "USD",
+			}
+
+			account, err := testQueries.CreateAccount(context.Background(), arg)
+			Expect(err).To(BeNil())
+			Expect(account.Balance).To(Equal(arg.Balance))
+			Expect(account.Currency).To(Equal(arg.Currency))
+			Expect(account.Owner).To(Equal(arg.Owner))
+			Expect(account.ID).NotTo(BeZero())
+			Expect(account.CreatedAt).NotTo(BeZero())
+		})
+
+	})
+})
