575aa6a08f5b9bc2c1b670c4d8a4783130e98587
Yibo Fu <yibomus@gmail.com>
2023-07-07 01:51:06 -0700

    add UT for SQL transfer

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -1,1 +1,105 @@
 package db
+
+import (
+	"context"
+	"github.com/Petatron/bank-simulator-backend/db/util"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Operation", func() {
+
+	BeforeEach(func() {
+
+	})
+
+	AfterEach(func() {
+
+	})
+
+	Context("Transfer Operations", func() {
+		It("Test CreateTransfer", func() {
+			testOwnerName := util.GetRandomOwnerName()
+			testBalance := util.GetRandomMoneyAmount()
+			testCurrency := util.GetRandomCurrency()
+			arg := CreateAccountParams{
+				Owner:    testOwnerName,
+				Balance:  testBalance,
+				Currency: testCurrency,
+			}
+
+			account, _ := testQueries.CreateAccount(context.Background(), arg)
+
+			transfer, err := testQueries.CreateTransfer(context.Background(), CreateTransferParams{
+				FromAccountID: account.ID,
+				ToAccountID:   account.ID,
+				Amount:        100,
+			})
+			Expect(err).To(BeNil())
+			Expect(transfer.Amount).To(Equal(int64(100)))
+		})
+
+		It("Test GetTransfer", func() {
+			testOwnerName := util.GetRandomOwnerName()
+			testBalance := util.GetRandomMoneyAmount()
+			testCurrency := util.GetRandomCurrency()
+			arg := CreateAccountParams{
+				Owner:    testOwnerName,
+				Balance:  testBalance,
+				Currency: testCurrency,
+			}
+
+			account, _ := testQueries.CreateAccount(context.Background(), arg)
+
+			transferAmount := util.GetRandomMoneyAmount()
+
+			transfer, err := testQueries.CreateTransfer(context.Background(), CreateTransferParams{
+				FromAccountID: account.ID,
+				ToAccountID:   account.ID,
+				Amount:        transferAmount,
+			})
+			Expect(err).To(BeNil())
+			Expect(transfer.Amount).To(Equal(transferAmount))
+		})
+
+		It("Test ListTransfers", func() {
+			testOwnerName := util.GetRandomOwnerName()
+			testBalance := util.GetRandomMoneyAmount()
+			testCurrency := util.GetRandomCurrency()
+			arg := CreateAccountParams{
+				Owner:    testOwnerName,
+				Balance:  testBalance,
+				Currency: testCurrency,
+			}
+
+			account, _ := testQueries.CreateAccount(context.Background(), arg)
+			transferAmount := util.GetRandomMoneyAmount()
+
+			_, err := testQueries.CreateTransfer(context.Background(), CreateTransferParams{
+				FromAccountID: account.ID,
+				ToAccountID:   account.ID,
+				Amount:        transferAmount,
+			})
+			Expect(err).To(BeNil())
+
+			_, err = testQueries.CreateTransfer(context.Background(), CreateTransferParams{
+				FromAccountID: account.ID,
+				ToAccountID:   account.ID,
+				Amount:        transferAmount,
+			})
+			Expect(err).To(BeNil())
+
+			transfers, err := testQueries.ListTransfers(context.Background(), ListTransfersParams{
+				FromAccountID: account.ID,
+				ToAccountID:   account.ID,
+				Limit:         5,
+				Offset:        0,
+			})
+			Expect(err).To(BeNil())
+			Expect(len(transfers)).To(Equal(2))
+
+		})
+
+	})
+
+})
