d04990614be66ee02cf04968cfedeea2b9f0e571
Yibo Fu <yibomus@gmail.com>
2023-07-20 18:40:05 -0700

    create main.go with server port

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
-package bank_simulator_backend
+package main
+
+import (
+	"database/sql"
+	"github.com/Petatron/bank-simulator-backend/api"
+	db "github.com/Petatron/bank-simulator-backend/db/sqlc"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:password@localhost:5432/bank_simulator?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
+}
