07bc0d8a4a314fbda86dd7034d367fa2637438f0
Yibo Fu <yibomus@gmail.com>
2023-07-26 03:14:27 -0700

    implement LoadConfig via Viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,25 +2,25 @@ package db
 
 import (
 	"database/sql"
+	"github.com/Petatron/bank-simulator-backend/db/util"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:password@localhost:5432/bank_simulator?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
 	if err != nil {
-		log.Fatal("cannot connect to db", err)
+		log.Fatal("Unable to load project config with error: ", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to Database with error: ", err)
 	}
 
 	testQueries = New(testDB)
