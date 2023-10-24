1deeb72dd91a9737d74a46a93a4fcf6e4d37d558
Yibo Fu <yibomus@gmail.com>
2023-10-23 17:18:31 -0700

    add AWS RDS and secret manager

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ set -e
 
 # Start the first process
 echo "Running db migration"
+# shellcheck disable=SC2039
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 # Start the second process
